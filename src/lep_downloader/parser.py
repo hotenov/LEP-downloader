2779b33a49b397acb8c56e60e1d7c58b80bd03dc
Artem Hotenov <qa@hotenov.com>
2021-09-17 11:12:59 +0300

    refactor(parser): :construction: Update two functions for parsing all links and getting text link by href

--- src/lep_downloader/parser.py
+++ src/lep_downloader/parser.py
@@ -1,9 +1,9 @@
 """LEP module for parsing logic."""
 import copy
 import re
-from typing import List, Tuple
+from typing import Any, List, Tuple
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
 
 from lep_downloader import config as conf
@@ -12,41 +12,35 @@ from lep_downloader import config as conf
 deleted_links = []
 regex = conf.EPISODE_LINK_RE
 ep_pattern = re.compile(regex, re.IGNORECASE)
+s = requests.Session()
 
 
-def get_lep_archive_page(archive_url: str) -> str:
+def get_web_page_html_text(archive_url: str, session: requests.Session) -> str:
     """Return HTML text of LEP archive page."""
-    with requests.Session() as s:
-        req = s.get(archive_url)
+    with session:
+        req = session.get(archive_url, timeout=(6, 33))
         req.encoding = "utf-8"
     return req.text
 
 
-def get_all_links_from_tag_with_class(
-    soup_obj: BeautifulSoup,
-    tag_name: str,
-    class_name: str,
-) -> List[str]:
+def get_all_links_from_soup(soup_obj: BeautifulSoup) -> List[str]:
     """Return list of links from HTML block."""
-    tag_entry_content = soup_obj.find(tag_name, class_= class_name)
     all_links: List[str] = []
-    all_link_tags = tag_entry_content.find_all("a")
-    for link in all_link_tags:
-        all_links.append(link["href"].strip())
+    all_tags_a = soup_obj("a")
+    for tag_a in all_tags_a:
+        all_links.append(tag_a["href"].strip())
 
     return all_links
 
 
-def replace_misspelled_link(all_links: List) -> List:
+def replace_misspelled_link(soup_obj: BeautifulSoup) -> BeautifulSoup:
     """Replace link with '.ukm' misspelled LTD"""
-    links_without_misspelled_ltd = copy.deepcopy(all_links)
-    try:
-        bad_index = links_without_misspelled_ltd.index("https://teacherluke.co.ukm/2012/08/06/london-olympics-2012/")
-        links_without_misspelled_ltd[bad_index] = "https://teacherluke.co.uk/2012/08/06/london-olympics-2012/"
-    except ValueError:
-        # Here, no need to handle it.
-        pass
-    return links_without_misspelled_ltd
+    modified_soup = copy.copy(soup_obj)  # TODO: Really needs to copy?
+    misspelled_tag_a = modified_soup.find("a", href="https://teacherluke.co.ukm/2012/08/06/london-olympics-2012/")
+    if misspelled_tag_a:
+        misspelled_tag_a["href"] = "https://teacherluke.co.uk/2012/08/06/london-olympics-2012/"
+    del misspelled_tag_a
+    return modified_soup
 
 
 def remove_irrelevant_links(links: List) -> List:
@@ -70,6 +64,23 @@ def remove_not_episode_links(links: List) -> List:
     return result
 
 
+def get_links_text_by_href(
+    soup_obj: BeautifulSoup,
+    links: List[str],
+) -> List[str]:
+    """Return text of <a></a> tag by its href attribute."""
+    link_strings = []
+    for url in links:
+        a_tag = soup_obj.find("a", href=url)
+        if url in [*conf.LINK_TEXTS_MAPPING]:
+            link_string = conf.LINK_TEXTS_MAPPING[url]
+        else:
+            link_string = " ".join([text for text in a_tag.stripped_strings])
+        link_strings.append(link_string)
+
+    return link_strings
+
+
 def substitute_short_links(unique_links: List) -> List:
     """Return list of links with final location for short links."""
     final_links = copy.deepcopy(unique_links)
@@ -85,16 +96,37 @@ def substitute_short_links(unique_links: List) -> List:
 
 def get_archive_parsing_results(archive_url: str) -> Tuple:
     """Return Tuple with valid episode links and discarded links."""
-    html_page = get_lep_archive_page(archive_url)
-    soup_obj = BeautifulSoup(html_page, "lxml")
-    all_links = get_all_links_from_tag_with_class(soup_obj, "div", "entry-content")
-    cleaned_links = replace_misspelled_link(all_links)
-    cleaned_links = remove_irrelevant_links(cleaned_links)
-    cleaned_links = remove_not_episode_links(cleaned_links)
-    # Get unique links with preserved order for Python 3.7+
-    unique_links = list(dict.fromkeys(cleaned_links))
-    final_list = substitute_short_links(unique_links)
-    parsing_result = (final_list, deleted_links)
-    return parsing_result
+    html_page = get_web_page_html_text(archive_url, s)
+    only_div_entry_content = SoupStrainer("div", class_="entry-content")
+    soup_div = BeautifulSoup(html_page, "lxml", parse_only=only_div_entry_content)
+
+    if soup_div:
+        modified_soup_div = replace_misspelled_link(soup_div)
+        all_links = get_all_links_from_soup(modified_soup_div)
+        cleaned_links = remove_irrelevant_links(all_links)
+        cleaned_links = remove_not_episode_links(cleaned_links)
+
+        # Get unique links with preserved order for Python 3.7+
+        unique_links = list(dict.fromkeys(cleaned_links))
+
+        # Get list of 'link labeles'
+        link_strings = get_links_text_by_href(modified_soup_div, unique_links)
+
+        final_list = substitute_short_links(unique_links)
+        parsing_result = (final_list, deleted_links, link_strings)
+        return parsing_result
+    else:
+        print("[ERROR] Can't parse this page: Main <div> is not found")
+        return None
 
 
+def parse_single_page(url: str, session: requests.Session) -> Any:
+    """Returns result of parsing of single page."""
+    req = session.get(url, timeout=(3.05, 27))
+    req.encoding = "utf-8"
+    html_text = req.text
+
+    soup_obj = BeautifulSoup(html_text, "lxml")
+    page_title = soup_obj.title.string
+    result = page_title
+    return result
