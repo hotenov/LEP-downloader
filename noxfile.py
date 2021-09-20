6207855477829eb3baafe47b9d5424e29a635ed5
Artem Hotenov <qa@hotenov.com>
2021-09-20 16:22:10 +0300

    ci: :wrench: Add installation of 'requests_mock' into 'typeguard' Nox session

--- noxfile.py
+++ noxfile.py
@@ -154,7 +154,7 @@ def coverage(session: Session) -> None:
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard", "pygments")
+    session.install("pytest", "typeguard", "pygments", "requests_mock")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
