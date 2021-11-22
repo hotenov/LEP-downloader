1614f8177c0e40a368fa1cf8ba328b901339c9e9
Artem Hotenov <qa@hotenov.com>
2021-11-22 17:55:38 +0300

    test(console): :white_check_mark: Add test to check --version option

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,14 @@ def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
+
+
+def test_cli_prints_version(runner: CliRunner) -> None:
+    """It prints version."""
+    result = runner.invoke(
+        __main__.main,
+        ["--version"],
+        prog_name="lep-downloader",
+    )
+    assert result.exit_code == 0
+    assert "lep-downloader, version 3.0.0a" in result.output
