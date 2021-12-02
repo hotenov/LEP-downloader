fe666184dd94bca9d590a50c2af08848909ac632
Artem Hotenov <qa@hotenov.com>
2021-12-02 11:59:29 +0300

    feat(console): :construction: Call parsing method for 'parse' command withot options

--- src/lep_downloader/commands/parse.py
+++ src/lep_downloader/commands/parse.py
@@ -1,8 +1,13 @@
 """Parse command."""
 import click
 
+from lep_downloader import config as conf
+from lep_downloader import parser
+
 
 @click.command(name="parse")
 def cli() -> None:
     """Parses LEP archive web page."""
-    click.echo("parse_cmd() was executed...")
+    click.echo("'parse' command was executed...")
+
+    parser.do_parsing_actions(conf.JSON_DB_URL, conf.ARCHIVE_URL)
