4cfd6ad3e5cf7321f6d8fb2ab2aea086bdcbd09c
Artem Hotenov <qa@hotenov.com>
2021-12-27 10:37:08 +0300

    refactor(downloader): :poop: Attempt to implement OOP for downloader module
      Add class Downloader with shared dictonaries
      Add function to constract bunch of links for downloading

--- src/lep_downloader/downloader.py
+++ src/lep_downloader/downloader.py
@@ -22,6 +22,7 @@
 """LEP module for downloading logic."""
 import re
 from pathlib import Path
+from typing import ClassVar
 from typing import Dict
 from typing import List
 from typing import Tuple
@@ -29,8 +30,9 @@ from typing import Tuple
 import requests
 
 from lep_downloader import config as conf
-from lep_downloader.lep import LepEpisode
+from lep_downloader.exceptions import NoEpisodesInDataBase
 from lep_downloader.lep import Lep
+from lep_downloader.lep import LepEpisode
 
 
 DataForEpisodeAudio = List[Tuple[str, str, List[List[str]], bool]]
@@ -44,18 +46,18 @@ INVALID_PATH_CHARS_PATTERN = re.compile(conf.INVALID_PATH_CHARS_RE)
 
 # STATISTICS (LOG) DICTIONARIES #
 
-successful_downloaded: Dict[str, str] = {}
-unavailable_links: Dict[str, str] = {}
-already_on_disc: Dict[str, str] = {}
-duplicated_links: Dict[str, str] = {}
+# successful_downloaded: Dict[str, str] = {}
+# unavailable_links: Dict[str, str] = {}
+# already_on_disc: Dict[str, str] = {}
+# duplicated_links: Dict[str, str] = {}
 
 
-def select_all_audio_episodes(
-    db_episodes: List[LepEpisode],
-) -> List[LepEpisode]:
-    """Return filtered list with AUDIO episodes."""
-    audio_episodes = [ep for ep in db_episodes if ep.post_type == "AUDIO"]
-    return audio_episodes
+# def select_all_audio_episodes(
+#     db_episodes: List[LepEpisode],
+# ) -> List[LepEpisode]:
+#     """Return filtered list with AUDIO episodes."""
+#     audio_episodes = [ep for ep in db_episodes if ep.post_type == "AUDIO"]
+#     return audio_episodes
 
 
 def get_audios_data(audio_episodes: List[LepEpisode]) -> DataForEpisodeAudio:
@@ -147,12 +149,48 @@ def download_and_write_file(
         return False
 
 
+class Downloader(Lep):
+    """Represent downloader object."""
+
+    downloaded: ClassVar[Dict[str, str]] = {}
+    not_found: ClassVar[Dict[str, str]] = {}
+    existed: ClassVar[Dict[str, str]] = {}
+
+    # def __init__(self, url: str = "", session: requests.Session = None) -> None:
+    #     """Initialize Downloader instance.
+
+    #     Args:
+    #         url (str): URL for parsing.
+    #         session (requests.Session): Requests session object
+    #             if None, get default global session.
+    #     """
+    #     self.url = url
+    #     self.session = session if session else Lep.session
+
+
+def construct_audio_links_bunch(json_url: str) -> NamesWithAudios:
+    """Get and constract audio links with filenames for them."""
+    if not Lep.db_episodes:
+        Lep.db_episodes = Lep.get_db_episodes(json_url)
+
+    audio_links: NamesWithAudios = []
+    if Lep.db_episodes:
+        audio_episodes = Lep.db_episodes.filter_by_type("AUDIO")
+        only_audio_data = get_audios_data(audio_episodes)
+        audio_links = bind_name_and_file_url(only_audio_data)
+    return audio_links
+
+
 def download_files(
     downloads_bunch: NamesWithAudios,
     save_dir: Path,
     file_ext: str = ".mp3",
 ) -> None:
     """Download files from passed links bunch."""
+    if not downloads_bunch:
+        raise NoEpisodesInDataBase(
+            "JSON is available, but\nthere are NO episodes in this file. Exit."
+        )
     for item in downloads_bunch:
         file_stem: str = item[0]
         links: List[str] = item[1]
@@ -160,11 +198,11 @@ def download_files(
 
         primary_link = links[0]
         if Path(save_dir / filename).exists():
-            already_on_disc[primary_link] = filename
+            Downloader.existed[primary_link] = filename
             continue  # Skip already downloaded file on disc.
-        if primary_link in successful_downloaded:
-            duplicated_links[primary_link] = filename
-            continue  # Skip already processed URL.
+        # if primary_link in successful_downloaded:
+        #     duplicated_links[primary_link] = filename
+        #     continue  # Skip already processed URL.
 
         result_ok = download_and_write_file(
             primary_link,
@@ -173,7 +211,7 @@ def download_files(
             filename,
         )
         if result_ok:
-            successful_downloaded[primary_link] = filename
+            Downloader.downloaded[primary_link] = filename
         else:
             if len(links) > 1:  # Try downloading for auxiliary links
                 for aux_link in links[1:]:
@@ -181,10 +219,10 @@ def download_files(
                         aux_link, Lep().session, save_dir, filename
                     )
                 if not aux_result_ok:
-                    unavailable_links[aux_link] = filename
+                    Downloader.not_found[aux_link] = filename
                     print(f"[INFO]: Can't download: {filename}")
                 else:
-                    successful_downloaded[aux_link] = filename
+                    Downloader.downloaded[aux_link] = filename
             else:
-                unavailable_links[primary_link] = filename
+                Downloader.not_found[primary_link] = filename
                 print(f"[INFO]: Can't download: {filename}")
