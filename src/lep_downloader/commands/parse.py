8674835c38337864ac91774c37d68ed28e37781a
Artem Hotenov <qa@hotenov.com>
2021-12-27 12:28:57 +0300

    refactor(console): :goal_net: Add handling custom exceptions for 'parse' command

--- src/lep_downloader/commands/parse.py
+++ src/lep_downloader/commands/parse.py
@@ -3,6 +3,10 @@ import click
 
 from lep_downloader import config as conf
 from lep_downloader import parser
+from lep_downloader.exceptions import DataBaseUnavailable
+from lep_downloader.exceptions import NoEpisodeLinksError
+from lep_downloader.exceptions import NoEpisodesInDataBase
+from lep_downloader.exceptions import NotEpisodeURLError
 
 
 @click.command(name="parse")
@@ -10,4 +14,16 @@ def cli() -> None:
     """Parses LEP archive web page."""
     click.echo("'parse' command was executed...")
 
-    parser.do_parsing_actions(conf.JSON_DB_URL, conf.ARCHIVE_URL)
+    try:
+        parser.do_parsing_actions(conf.JSON_DB_URL, conf.ARCHIVE_URL)
+    except NotEpisodeURLError as ex:
+        click.echo(f"{ex.args[1]}:\n\t{ex.args[0]}")
+        click.echo("Archive page has invalid HTML content. Exit.")
+    except NoEpisodeLinksError as ex:
+        click.echo(f"{ex.args[1]}:\n\t{ex.args[0]}")
+        click.echo("Can't parse any episodes. Exit.")
+    except DataBaseUnavailable:
+        click.echo("JSON database is not available. Exit.")
+    except NoEpisodesInDataBase as ex:
+        click.echo(f"[WARNING]: JSON file ({conf.JSON_DB_URL}) has no valid episode objects.")
+        click.echo("\t" + ex.args[0])
