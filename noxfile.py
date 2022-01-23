4b72384e82f529898ee90b45421f71a56fa9128c
Artem Hotenov <qa@hotenov.com>
2022-01-23 12:10:47 +0300

    chore: :heavy_plus_sign: Add 'pytest-mock' (3.6.1) into dev dependencies
      Update 'noxfile.py' to install 'pytest-mock'

--- noxfile.py
+++ noxfile.py
@@ -129,7 +129,9 @@ def mypy(session: Session) -> None:
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
-    session.install("coverage[toml]", "pytest", "pygments", "requests_mock")
+    session.install(
+        "coverage[toml]", "pytest", "pygments", "requests_mock", "pytest-mock"
+    )
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
     finally:
@@ -154,7 +156,7 @@ def coverage(session: Session) -> None:
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard", "pygments", "requests_mock")
+    session.install("pytest", "typeguard", "pygments", "requests_mock", "pytest-mock")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
