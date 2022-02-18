8ff6643ba9f2a533f0f8d09fefb68056703e3b23
Artem Hotenov <qa@hotenov.com>
2022-02-18 07:10:16 +0300

    ci: :construction_worker: Enable 'safety' Nox session again (it passes with latest Python (3.10) and upgraded dependencies)

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ python_versions = ["3.10", "3.9", "3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
-    # "safety",
+    "safety",
     "mypy",
     "tests",
     "typeguard",
