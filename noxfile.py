ab249964233545dbe20266b1c35edfe29e631a8d
Artem Hotenov <qa@hotenov.com>
2024-08-09 20:44:05 +0300

    💚 chore(ci): Temporary completely OFF 'pre-commit' check

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ package = "lep_downloader"
 python_versions = ["3.10", "3.9", "3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
-    "pre-commit",
+    # "pre-commit",
     # "safety",
     "mypy",
     "tests",
